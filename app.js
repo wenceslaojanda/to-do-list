f645cc934419dd3178a1cab29ccc00e96ebeed33
wenceslaojanda <wenchojanda@gmail.com>
2022-04-09 17:32:12 -0300

    terminado practica lista de tareas con local storage

--- app.js
+++ app.js
@@ -27,23 +27,32 @@ const PintarDB = () => {
         arrayActividades = [];
     } else {
         arrayActividades.forEach(element => {
-            listaActividadesUI.innerHTML += `<div class="alert alert-danger" role="alert">
-        <i class="material-icons float-left mr-2">
-          accessibility
-        </i>
-        <b>${element.actividad}</b> - ${element.estado}
-        <span class="float-right">
-          <i class="material-icons">
-            done
-          </i>
-          <i class="material-icons">
-            delete
-          </i>
-        </span>
-      </div>`
+            if(element.estado){
+              listaActividadesUI.innerHTML += `<div class="alert alert-success" role="alert"><i class="material-icons float-left mr-2">accessibility</i><b>${element.actividad}</b> - ${element.estado}<span class="float-right"><i class="material-icons">done</i><i class="material-icons">delete</i></span></div>`
+            } else {
+             listaActividadesUI.innerHTML += `<div class="alert alert-danger" role="alert"><i class="material-icons float-left mr-2">accessibility</i><b>${element.actividad}</b> - ${element.estado}<span class="float-right"><i class="material-icons">done</i><i class="material-icons">delete</i></span></div>`
+            }
         });
     }
+}
 
+
+const EliminarDB = (actividad) => {
+  let indexArray;  
+  arrayActividades.forEach((elemento, index) => {
+    if(elemento.actividad === actividad){
+        indexArray = index;
+    }
+  });
+
+  arrayActividades.splice(indexArray, 1);
+  GuardarDB();
+}
+
+const EditarDB = (actividad) => {
+  let indexArray = arrayActividades.findIndex((elemento) => elemento.actividad === actividad);
+  arrayActividades[indexArray].estado = true;
+  GuardarDB();
 }
 
 //EventListener
@@ -58,3 +67,21 @@ formularioUI.addEventListener('submit', (e) =>{
 });
 
 document.addEventListener('DOMContentLoaded', PintarDB);
+
+listaActividadesUI.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if(e.target.innerHTML === 'done' || e.target.innerHTML === 'delete'){
+      let texto = e.path[2].childNodes[1].innerHTML;
+      if(e.target.innerHTML === 'delete'){
+        //Accion de eliminar
+        EliminarDB(texto);
+      }
+      if(e.target.innerHTML === 'done'){
+        //Accion de editar
+        EditarDB(texto);
+      }
+
+    }
+
+});
